fee24f16949709a1215d1f1d299f2ac02e6c37fe
zhouziqi_global <zhouziqi@gmail.com>
2019-05-09 01:20:08 +0800

    配置文件修改

--- server-hystrix-dashboard/src/main/java/com/xbd/server/hystrix/dashboard/ServerHystrixDashboardApplication.java
+++ server-hystrix-dashboard/src/main/java/com/xbd/server/hystrix/dashboard/ServerHystrixDashboardApplication.java
@@ -1,7 +1,6 @@
 package com.xbd.server.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.cloud.netflix.turbine.EnableTurbine;
